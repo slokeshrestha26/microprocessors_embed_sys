e9146a1996a3c8effb1372574492d172ba4c5cdb
Sloke Shrestha <slokeshrestha131@gmail.com>
2021-02-03 14:57:15 -0600

    Creating file for learning struct and pointers in C

---
+++ C_files_micro/Learning Pointers and Struct/pointers_and_struct.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+//learning about pointers in  c
+	int var1;
+	char var2[10];
+
+	printf("Address of var1: %p\n", &var1);
+	printf("Address of var2: %p\n", &var2);
+
+	return 0;
+}
+
